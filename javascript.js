3fb315a27f1b697a095480937a1f94d3e89e7e33
harikrishna229 <yeturuharikrishna@gmail.com>
2024-02-28 22:24:05 +0530

    9th commit

--- javascript.js
+++ javascript.js
@@ -48,36 +48,75 @@
 //     // Append the new element to the parent element
 //     parentElement.append(newElement);
 // }
-function toggleSections() {
-    var middleSection = document.getElementsByClassName("middlesection")[0];
-    var leftSection = document.querySelector('.leftsection');
-    var rightSection = document.querySelector('.rightsection');
-    var searchInput = document.querySelector('.searchinput');
-    var searchBtn = document.querySelector('.searchbtn');
-var searchicon =document.querySelector('.searchicon');
+$(document).ready(function() {
+    $(".searchbtn").click(function(e) {
+        if (window.innerWidth <= 510) {
+            $(".leftsection").css("display", "none");
+            $(".rightsection").css("display", "none");
+            $(".searchinput").css("display", "flex");
+            $(".searchbtn").addClass("searchbtnmobile");
+            $(".back-button").css("display", "flex");
+        }
+        
+    });
+    $(".back-button").click(function (e) { 
+                $(".leftsection").css("display", "flex");
+                $(".rightsection").css("display", "flex");
+                $(".searchinput").css("display", "none");
+                $(".searchbtn").removeClass("searchbtnmobile");
+                $(".back-button").css("display", "none");
+        
+    });
+    function handleResize(){
+        if (window.innerWidth >510) {
+                    $(".leftsection").css("display", "flex");
+                    $(".rightsection").css("display", "flex");
+                    $(".searchinput").css("display", "flex");
+                    $(".searchbtn").removeClass("searchbtnmobile");
+        }
+        if (window.innerWidth <=510) {
+            
+            $(".searchinput").css("display", "none");
+        
+}
+    }
+
+    $(window).resize(handleResize);
+
+
+  });
+
+//  
+// function toggleSections() {
+//     var middleSection = document.getElementsByClassName("middlesection")[0];
+//     var leftSection = document.querySelector('.leftsection');
+//     var rightSection = document.querySelector('.rightsection');
+//     var searchInput = document.querySelector('.searchinput');
+//     var searchBtn = document.querySelector('.searchbtn');
+// var searchicon =document.querySelector('.searchicon');
     
 
-    if (window.innerWidth <= 510) {
-        if (leftSection.style.display==="none") {
-            middleSection.style.display = "flex"
-            leftSection.style.display = "flex";
-            searchInput.style.display = "none";
-            rightSection.style.display = "flex";
-            searchBtn.style.cssText = "background-color:white;height:28px;border: none; width:30px;";
+//     if (window.innerWidth <= 510) {
+//         if (leftSection.style.display==="none") {
+//             middleSection.style.display = "flex"
+//             leftSection.style.display = "flex";
+//             searchInput.style.display = "none";
+//             rightSection.style.display = "flex";
+//             searchBtn.style.cssText = "background-color:white;height:28px;border: none; width:30px;";
             
-        }
-        else{
-            middleSection.style.display = "flex";
-            leftSection.style.display = "none";
-            rightSection.style.display = "none";
-            searchInput.style.display = "flex";
-            searchBtn.style.cssText = "background-color:rgb(240, 240, 240);height:28px;border: 1px solid rgb(198, 192, 192); width:40px;";
-            searchicon.style.cssText="margin-top: 3px";
+//         }
+//         else{
+//             middleSection.style.display = "flex";
+//             leftSection.style.display = "none";
+//             rightSection.style.display = "none";
+//             searchInput.style.display = "flex";
+//             searchBtn.style.cssText = "background-color:rgb(240, 240, 240);height:28px;border: 1px solid rgb(198, 192, 192); width:40px;";
+//             searchicon.style.cssText="margin-top: 3px";
 
-        }
+//         }
        
         
 
-    }
-}
+//     }
+// }
 
