aee507b2336fd74d1f25e18662e71f52a12f81a6
harikrishna229 <yeturuharikrishna@gmail.com>
2024-03-02 15:24:27 +0530

    21 commit

--- javascript.js
+++ javascript.js
@@ -101,6 +101,8 @@ $(document).ready(function() {
    $( ".searchinput" ).autocomplete({
     source: availableTags
   });
+
+  
    
 
 
@@ -111,6 +113,61 @@ $(document).ready(function() {
 
 
   });
+//   var videos=document.querySelector("section");
+//   fetch("https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=15&playlistId=UUE750MuIvjaYeD5aGUjJcCQ&key=AIzaSyCFwuqhXldhb4KN8iSdZLMUyZgP_KZi8hM")
+//   .then(res=> res.json())
+//   .then(data=>{
+//      //videos.innerHTML=data.items[0].snippet.title
+//      data.items.forEach(element => {
+//         videos.innerHTML +=`
+//         <a href="https://www.youtube.com/watch?v=${element.snippet.resourceId.videoId}" class="yt-videos" >
+//             <img src="${element.snippet.thumbnails.maxres.url}" />
+//             <h2>${element.snippet.title}</h2>
+//         </a>`
+//      });
+//        console.log(data);
+
+//   })
+
+var videos=document.querySelector(".videogrid");
+fetch("https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=15&playlistId=UUE750MuIvjaYeD5aGUjJcCQ&key=AIzaSyCFwuqhXldhb4KN8iSdZLMUyZgP_KZi8hM")
+.then(res=> res.json())
+.then(data=>{
+
+    
+   //videos.innerHTML=data.items[0].snippet.title
+//    data.items.forEach(element => {
+//       videos.innerHTML +=`
+//       <a href="https://www.youtube.com/watch?v=${element.snippet.resourceId.videoId}" class="yt-videos" >
+//           <img src="${element.snippet.thumbnails.maxres.url}" />
+//           <h2>${element.snippet.title}</h2>
+//       </a>`
+//    });
+    data.items.forEach(element => {
+     videos.innerHTML+=`<div class="container1">
+                                <div>
+                                
+                                </div>
+                                <div>
+                                <a href="https://www.youtube.com/watch?v=${element.snippet.resourceId.videoId}" class="yt-videos" ><img class="thumbnail" src="${element.snippet.thumbnails.maxres.url}"></a>
+                                </div>
+                                <div class="videoinfo">
+                                    <div class="horzlay1">
+                                        <img class="channelogo" src="images/video images/channel-1.jpg">
+                                    </div>
+                                    <div class="horzlay2" >
+                                        <div><p class="title">${element.snippet.title}</p></div>
+                                        <div><p class="author">Ishan Sharma</p></div>
+                                        <div><p class="stats">364K views 3 months ago</p></div>
+                                    </div>
+                                
+                                </div>
+                                
+                            </div>`
+
+     });
+
+})
 
 
 
